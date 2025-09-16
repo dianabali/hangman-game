048ef70c9d231d77f42513f1b2dd6ec73e8ce345
Diana Bali <dianabali64@gmail.com>
2025-09-16 13:15:24 +0200

    Initial commit

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ const wrongLetters = [];
 function displayWord() {
     wordEl.innerHTML = `
         ${selectedWord
-            .split('') // Turn the word into an array of letters. Map letters to span elements
+            // Turn the word into an array of letters. Map letters to span elements
+            .split('') 
             .map(letter => ` 
                 <span class="letter">${correctLetters.includes(letter) ? letter : ''}</span>
             `).join('')}
@@ -27,11 +28,52 @@ function displayWord() {
     const innerWord = wordEl.innerText.replace(/\n/g, ''); // Remove new line characters
 
     if (innerWord === selectedWord) { // Check if the user has won
-        finalMessage.innerText = 'Yayyyy! You won! 😊';
+        finalMessage.innerText = 'Yayyyy! You won!😊';
         popup.style.display = 'flex';
     }
 }
 
+// Update the wrong letters
+function displayWrongLetters() {
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `; 
+
+
+// Show notification
+function showNotification() {
+    notification.classList.add('show');
+
+    // Hide the notification after 2 seconds
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener('keydown', e => {
+    if (e.keyCode >= 65 && e.keyCode <= 90) { // Check if the key pressed is a letter
+        const letter = e.key; // Get the letter pressed 
+
+        if (selectedWord.includes(letter)) { // Check if the letter is in the word
+            if (!correctLetters.includes(letter)) { // Check if the letter has not been guessed already
+                correctLetters.push(letter);
+                displayWord();
+            } else {
+                showNotification();
+            }
+        } else {
+            if (!wrongLetters.includes(letter)) { // Check if the letter has not been guessed already
+                wrongLetters.push(letter);
+                displayWrongLetters();
+            } else {
+                showNotification();
+            }
+        }
+    }
+});
+
 displayWord();
 
-// 6 
+
